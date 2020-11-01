042ce6b54270b624c55384ebb6753034d9738268
Mariana Guzmán <mggzmn91@gmail.com>
2020-11-01 17:52:32 -0600

    Lesson 5-14 Calculated added and burned calories

--- js/app.js
+++ js/app.js
@@ -11,13 +11,28 @@ var caloriesController = (function() {
         this.calories = calories;
         this.quantity = quantity;
     };
+    var totalCalories = function(type) {
+        var total = 0;
+        storage.allCalories[type].forEach(function(current) {
+            total += (current.calories) * (current.quantity);
+        });
+        storage.all[type] = total;
+    };
     var storage = {
         allCalories: {
             add: [],
             burn: []
-        }
-
+        },
+        all: {
+            add: 0,
+            burn: 0
+        },
+        totalCaloriesPerDay: 0,
+        minCalPerDay: 2000,
+        percentage: 0,
+        remain: 0
     };
+    //PUBLIC
     return {
         addCalory: function({ type, description, calories, quantity }) {
             var newCalory, id, caloryLength, calArray;
@@ -37,6 +52,22 @@ var caloriesController = (function() {
             storage.allCalories[type].push(newCalory);
             return newCalory;
         },
+        computedCalories: function() {
+            totalCalories('add');
+            totalCalories('burn');
+            storage.totalCaloriesPerDay = storage.all.add - storage.all.burn;
+            storage.remain = estorage.minCalPerDay - storage.totalCaloriesPerDay;
+            storage.percentage = Math.round((storage.totalCaloriesPerDay / storage.minCalPerDay) * 100);
+        },
+        returnCalories: function() {
+            return {
+                totalCalories: storage.totalCaloriesPerDay,
+                totalAdded: storage.all.add,
+                totalBurned: storage.all.burn,
+                percentage: storage.percentage,
+                remaining: storage.remain
+            }
+        },
         result: function() {
             return storage;
         }
@@ -52,6 +83,8 @@ var AppUiController = (function() {
         addBtnClass: '.showForm__add-btn',
         foodSliderClass: '.food__slider',
         exerciseClass: '.exercise__slider',
+        foodBtn: '.btn-panel__food--apple',
+        excerciseBtn: '.btn-panel__food--fire'
 
     };
     return {
@@ -67,18 +100,24 @@ var AppUiController = (function() {
             return htmlClassNames;
         },
         addItem: function(itemObj, type) {
-            var markup, editedMarkup, sliderContainer;
+            var markup, editedMarkup, sliderContainer, notification;
             if (type === "add") {
-            	sliderContainer = htmlClassNames.foodSliderClass;
+                sliderContainer = htmlClassNames.foodSliderClass;
+                notification = '<i class="fas fa-apple-alt"></i><span class="btn-panel__food--text">+ Food</span><div class="circle-container"><div class="pulsate"></div><div class="circle"></div></div>'
                 markup = '<div class="food-container__foods" id="add-%id%"><div class="food-container__foods--name">%description%</div><div class="food-container__foods--calBalance">Calorie Balance<span class="food-container__foods--cal">%calories% Cal</span><div class="food-container__foods--bar"></div></div><div class="food-container__foods--delete"><button class="btn-deleteItem"><i class="far fa-times-circle"></i></button></div></div>';
+                document.querySelector(htmlClassNames.excerciseBtn).innerHTML = '<i class="fas fa-fire"></i><span class="btn-panel__food--text exercise-burn-text"> - Exercise</span>'
+                document.querySelector(htmlClassNames.foodBtn).innerHTML = notification;
             } else if (type === "burn") {
-            	sliderContainer = htmlClassNames.exerciseClass;
+                sliderContainer = htmlClassNames.exerciseClass;
+                notification = '<i class="fas fa-fire"></i><span class="btn-panel__food--text exercise-burn-text"> - Exercise</span><div class="circle-container"><div class="pulsate"></div><div class="circle"></div></div> '
                 markup = '<div class="food-container__foods running-container__name" id="burn-%id%"><div class="food-container__foods--name running-container__name">%description%</div><div class="food-container__foods--calBalance running__calBalance">Calorie Burned<span class="food-container__foods--cal  running__calBalance--cal"> -%calories%Cal</span><div class="food-container__foods--bar  running__calBalance--bar"></div></div><div class="food-container__foods--delete running-container__delete"><button class="btn-deleteItem"><i class="far fa-times-circle"></i></button></div></div>';
+                document.querySelector(htmlClassNames.foodBtn).innerHTML = '<i class="fas fa-apple-alt"></i><span class="btn-panel__food--text">+ Food</span>';
+                document.querySelector(htmlClassNames.excerciseBtn).innerHTML = notification;
             }
             editedMarkup = markup.replace('%id%', itemObj.id);
             editedMarkup = editedMarkup.replace('%description%', itemObj.description);
             editedMarkup = editedMarkup.replace('%calories%', itemObj.calories);
-            document.querySelector(sliderContainer).insertAdjacentHTML('beforeend',editedMarkup);
+            document.querySelector(sliderContainer).insertAdjacentHTML('beforeend', editedMarkup);
         }
     }
 })();
@@ -100,6 +139,7 @@ var MainController = (function(caloriesCtrl, AppUICtrl) {
             }
         });
     };
+
     var addCalories = function() {
         var getFormInput, newAddCalories;
         getFormInput = AppUICtrl.getFormData();
@@ -107,8 +147,17 @@ var MainController = (function(caloriesCtrl, AppUICtrl) {
             newAddCalories = caloriesCtrl.addCalory(getFormInput);
             console.log(caloriesCtrl.result());
             AppUICtrl.addItem(newAddCalories, getFormInput.type);
+            calculateCalories();
         }
     }
+
+    var calculateCalories = function() {
+        var calories;
+        caloriesCtrl.computedCalories();
+        calories = caloriesCtrl.returnCalories();
+
+    }
+    //public
     return {
         initialize: function() {
             groupEventListeners();
